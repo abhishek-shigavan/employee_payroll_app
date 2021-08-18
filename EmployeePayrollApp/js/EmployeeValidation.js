db45e608c0a25bf215342c3fb907e6bb28864ce5
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-08-19 01:22:28 +0530

    Refactor: Adding functionality to reset form using reset button

--- EmployeePayrollApp/js/EmployeeValidation.js
+++ EmployeePayrollApp/js/EmployeeValidation.js
@@ -78,4 +78,32 @@ function createAndUpdateStorage(employeePayrollData) {
    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
- 
+const resetForm = () => {
+   setValue('#name','');
+   unsetSelectedValues('[name=profile]');
+   unsetSelectedValues('[name=gender]');
+   unsetSelectedValues('[name=department]');
+   setValue('#salary','');
+   setValue('#notes','');
+   setValue('#day','Day');
+   setValue('#month','Month');
+   setValue('#year','Year');
+   setTextValue('.salary_Output','4000000');
+};
+
+const unsetSelectedValues = (propertyValue) => {
+   let allItems = document.querySelectorAll(propertyValue);
+   allItems.forEach(item => {
+      item.checked = false;
+   });
+}
+
+const setTextValue = (id, value) => {
+   const element = document.querySelector(id);
+   element.textContent = value;
+}
+
+const setValue = (id, value) => {
+   const element = document.querySelector(id);
+   element.value = value;
+} 
